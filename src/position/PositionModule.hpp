7d1d9956ed52041da71fce490428f8fe142f79c3
Sebastian Schmidt <isibboi@gmail.com>
2014-01-30 18:23:28 +0100

    First version of taking calibration picture.

--- src/position/PositionModule.hpp
+++ src/position/PositionModule.hpp
@@ -2,11 +2,15 @@
 #define POSITION_MODULE_HPP
 
 #include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+#include <vector>
+#include <inttypes.h>
 
 #include "IPositionReceiver.hpp"
 #include "control_application/StartCalibration.h"
 #include "control_application/TakeCalibrationPicture.h"
 #include "camera_application/PictureSendingActivation.h"
+#include "camera_application/Picture.h"
 
 class PositionModule {
 private:
@@ -14,6 +18,8 @@ private:
 	
 	// Calibration
 	bool isCalibrating;
+	std::vector<cv::Mat*> pictureCache;
+	std::vector<uint64_t> pictureTimes;
 	
 	// ROS network
 	ros::ServiceServer startCalibrationService;
@@ -21,9 +27,14 @@ private:
 	
 	ros::Publisher pictureSendingActivationPublisher;
 	
+	ros::Subscriber pictureSubscriber;
+	
+	int rosId;
+	
 	// ROS callbacks
 	bool startCalibrationCallback(control_application::StartCalibration::Request &req, control_application::StartCalibration::Response &res);
 	bool takeCalibrationPictureCallback(control_application::TakeCalibrationPicture::Request &req, control_application::TakeCalibrationPicture::Response &res);
+	void pictureCallback(const camera_application::Picture &msg);
 	
 public:
 	PositionModule(IPositionReceiver* receiver);
