dd98759f9ed8784ef614a64dde086952158ccb25
Sebastian Schmidt <isibboi@gmail.com>
2014-02-14 21:00:37 +0100

    Added documentation to MovementGenerator

--- test/helpers/MovementGenerator.hpp
+++ test/helpers/MovementGenerator.hpp
@@ -36,6 +36,23 @@ private:
 public:
 	/// Dumb constructor, would need setter to be useful.
 	MovementGenerator(IPositionReceiver *receiver);
+	
+	/**
+	 * Creates a new MovementGenerator object.
+	 * The quadcopters in the from and to vectors have the position in the vector as id.<br />
+	 * Example: The quadcopter flying from from[2] to to[2] has id 2.
+	 * 
+	 * @param receiver The position receiver to send the position data to.
+	 * @param from The start positions of the quadcopters.
+	 * @param to The end positions of the quadcopters.
+	 * @param errorRate The probability for a random error.
+	 * @param errorSize The maximum size of a random error. A random error ranges from -errorSize to errorSize and is added to the position.
+	 *                  For every component of the position vector a new random number is generated, but the random error is applied to
+	 *                  either all components of the position vector or none.
+	 * @param defaultErrorSize The error that is always applied.
+	 * @param steps The amount of steps to use for moving from the start to the end position.
+	 * @param stepsPerSecond The amount of steps to calculate per second.
+	 */
 	MovementGenerator(IPositionReceiver *receiver, std::vector<Vector> from, std::vector<Vector> to, double errorRate, double errorSize, double defaultErrorSize, int steps, double stepsPerSecond);
 	
 	/// Runs the generator synchronous (This method blocks until it finished)
