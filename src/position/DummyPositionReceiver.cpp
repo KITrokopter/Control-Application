da0004e6c008575d594801ea0fa42098fa9585c1
Sebastian Schmidt <isibboi@gmail.com>
2014-03-05 13:49:31 +0100

    Output calculated positions.

--- src/position/DummyPositionReceiver.cpp
+++ src/position/DummyPositionReceiver.cpp
@@ -1,6 +1,9 @@
 #include "DummyPositionReceiver.hpp"
 
+#include <ros/console.h>
+
 void DummyPositionReceiver::updatePositions(std::vector<Vector> positions, std::vector<int> ids, std::vector<int> updates)
 {
 	// Do nothing, this is just a dummy.
+	ROS_DEBUG("Id: %d Position: %d %d %d", ids[0], positions[0].getV1(), positions[0].getV2(), positions[0].getV3());
 }
