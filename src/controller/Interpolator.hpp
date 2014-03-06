b3258de0da27389ba7074dc7667f3c5d6551b052
linoba <dominik.kiefer@t-online.de>
2014-03-06 10:35:39 +0100

    TODOs

--- src/controller/Interpolator.hpp
+++ src/controller/Interpolator.hpp
@@ -1,14 +1,27 @@
 #ifndef INTERPOLATOR_HPP
 #define INTERPOLATOR_HPP
 
-#include <time.h>
 #include <cmath>
+#include <time.h>
+#include <MovementQuadruple.hpp>
 
 #define MAX_STEPS_IN_ADVANCE 1 	// How delayed is the input?
+
+/*
+ * TODO safe sent movementQuadruples in lists/ vectors ~ 10times/sec
+ * TODO give that data + positional information (latest x datasets?) to calculateNextMQ()
+ * TODO linear interpolation
+ * TODO test of speed, either optimize or save data-to-sent in advance
+ * TODO optimize interpolation (step size time-dependant)
+ * TODO optimize interpolation (replace linear function)
+ * 
+ */
 
 class Interpolator {
 public:
 	Interpolator(){};
+
+	MovementQuadruple calculateNextMQ();
 
 protected:
 
