dc1bcd7138291021bc9beb22e1764d948c74d07e
linoba <dominik.kiefer@t-online.de>
2014-03-14 14:04:12 +0100

    now with includes

---
+++ src/controller/CalculatorMoveUp.cpp
@@ -0,0 +1,15 @@
+#include "CalculatorMoveUp.hpp"
+#include "Controller.hpp"
+
+CalculatorMoveUp::CalculatorMoveUp
+{
+	this->chargeToThrust.insert( std::pair<double, int>(0, 60000));
+	this->chargeToThrust.insert( std::pair<double, int>(3, 10001));
+	
+	this->chargeToThrust.insert( std::pair<double, int>(1.5, 4001));
+}
+
+int CalculatorMoveUp::thrustStartLinear( double charge, int amount )
+{
+
+}
