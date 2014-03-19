4002eeaf50f6f5a13bd45600d3fe95cadc178985
kitrokopter <kitrokopter@kitrokopter-vm1.(none)>
2014-03-19 15:20:47 +0100

    uncommented

--- test/test_messages/alle/system_start.sh
+++ test/test_messages/alle/system_start.sh
@@ -2,7 +2,7 @@ rostopic pub -1 /System api_application/System "{header: auto, command: 1}"
 
 rosrun control_application SetQuadcopters.sh
 rosrun control_application SetFormation1.sh
-rosrun control_application BuildFormations.sh
+rosrun control_application BuildFormation.sh
 #. ../Controller/SetQuadcopters.sh
 #. ../Controller/SetFormation1.sh
 #. ../Controller/BuildFormation.sh
