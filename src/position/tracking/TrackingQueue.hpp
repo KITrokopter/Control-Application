0011a2030b6f4f96daa11e036c4647b158ccadb2
Sebastian Schmidt <isibboi@gmail.com>
2014-03-22 17:51:21 +0100

    Fixed TrackingQueue template

--- src/position/tracking/TrackingQueue.hpp
+++ src/position/tracking/TrackingQueue.hpp
@@ -7,18 +7,8 @@
 template <class T>
 class TrackingQueue {
 public:
-	// Template stuff
-	TrackingQueue()
-	{
-		size = 0;
-	}
-	
-	~TrackingQueue()
-	{
-		for (std::map<int, AbstractCameraQueue*>::iterator it = queues.begin(); it != queues.end(); it++) {
-			delete it->second;
-		}
-	}
+	TrackingQueue();
+	~TrackingQueue();
 	
 	void enqueue(std::vector<CameraData> data);
 	void enqueue(CameraData data);
@@ -33,3 +23,82 @@ private:
 	int rrIndex;
 	size_t size;
 };
+
+template <class T>
+TrackingQueue<T>::TrackingQueue()
+{
+	size = 0;
+}
+
+template <class T>
+TrackingQueue<T>::~TrackingQueue()
+{
+	for (std::map<int, AbstractCameraQueue*>::iterator it = queues.begin(); it != queues.end(); it++) {
+		delete it->second;
+	}
+}
+
+template <class T>
+void TrackingQueue<T>::enqueue(std::vector<CameraData> data)
+{
+	for (std::vector<CameraData>::iterator it = data.begin(); it != data.end(); it++) {
+		enqueue(*it);
+	}
+}
+
+template <class T>
+void TrackingQueue<T>::enqueue(CameraData data)
+{
+	if (queues.count(data.quadcopterId) == 0) {
+		queues[data.quadcopterId] = new T();
+		ids.push_back(data.quadcopterId);
+	}
+	
+	size++;
+}
+
+template <class T>
+size_t TrackingQueue<T>::getSize()
+{
+	return size;
+}
+
+template <class T>
+std::vector<CameraData> TrackingQueue<T>::dequeue()
+{
+	if (size == 0) {
+		return std::vector<CameraData>();
+	}
+	
+	int index = rrIndex;
+	
+	do {
+		if (queues[ids[index]]->dataAvailable()) {
+			break;
+		}
+		
+		index++;
+	} while (index != rrIndex);
+	
+	if (index == rrIndex) {
+		return std::vector<CameraData>();
+	} else {
+		rrIndex = (index + 1) % ids.size();
+		std::vector<CameraData> result = queues[ids[index]]->dequeue();
+		size -= result.size();
+		
+		return result;
+	}
+}
+
+template <class T>
+bool TrackingQueue<T>::dataAvailable()
+{
+	for (std::map<int, AbstractCameraQueue*>::iterator it = queues.begin(); it != queues.end(); it++) {
+		if (it->second->dataAvailable()) {
+			return true;
+		}
+	}
+	
+	return false;
+}
