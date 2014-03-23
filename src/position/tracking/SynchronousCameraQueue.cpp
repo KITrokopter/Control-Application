f825c7a221778552830f54f677f485121889c377
Sebastian Schmidt <isibboi@gmail.com>
2014-03-23 18:17:36 +0100

    First version of synchronous camera queue

--- src/position/tracking/SynchronousCameraQueue.cpp
+++ src/position/tracking/SynchronousCameraQueue.cpp
@@ -2,10 +2,12 @@
 
 #include "../../matlab/profiling.hpp"
 
-SynchronousCameraQueue::SynchronousCameraQueue(long int arrivalDelay, long int maxDelay)
+SynchronousCameraQueue::SynchronousCameraQueue(long int arrivalDelay, long int maxDelay, long maxGroupInterval)
 {
 	this->maxDelay = maxDelay;
 	this->arrivalDelay = arrivalDelay;
+	this->maxGroupInterval = maxGroupInterval;
+	
 	size = 0;
 }
 
@@ -45,5 +47,150 @@ void SynchronousCameraQueue::enqueueInternal(CameraData data)
 	tmp.push_back(b);
 	queue.merge(tmp);
 	
+	camNos.insert(data.camNo);
+	
 	size++;
+}
+
+std::vector<CameraData> SynchronousCameraQueue::dequeue()
+{
+	long int currentTime = getNanoTime();
+	
+	// Search for element which is older than maxDelay
+	for (std::list<Bucket>::iterator it = queue.end(); it != queue.begin(); it--) {
+		if (currentTime - it->arrivalTime > maxDelay) {
+			// element is overdue, so return group, even if it's invalid.
+			return searchGroup(it, currentTime, queue.begin(), queue.end()).data;
+		}
+	}
+	
+	Group result;
+	
+	for (std::list<Bucket>::iterator it = queue.begin(); it != queue.end(); it++) {
+		Group tmp = searchGroup(it, currentTime, queue.begin(), queue.end());
+		
+		if (tmp.valid && (!result.valid || tmp.value > result.value)) {
+			result = tmp;
+		}
+	}
+	
+	if (result.valid) {
+		return result.data;
+	} else {
+		return std::vector<CameraData>();
+	}
+}
+
+SynchronousCameraQueue::Group SynchronousCameraQueue::searchGroup(std::list<Bucket>::iterator it, long int currentTime, std::list<Bucket>::iterator begin, std::list<Bucket>::iterator end)
+{
+	Group result;
+	result.data.push_back(it->data);
+	
+	std::set<int> usedCamNos;
+	camNos.insert(it->data.camNo);
+	
+	// true if we found an element that is older than arrivalDelay
+	bool hasWaiting = currentTime - it->arrivalTime > arrivalDelay;
+	
+	// The borders of the time interval of the group we are constructing
+	long int minTime = it->data.time;
+	long int maxTime = it->data.time;
+	
+	// The current rand of the search area
+	std::list<Bucket>::iterator left = it;
+	std::list<Bucket>::iterator right = it;
+	
+	// Temp iterator to reduce the amount of if/else
+	std::list<Bucket>::iterator current = it;
+	
+	// Which side of the rand to go
+	int direction = 0; // -1 left, 1 right, 0 terminate
+	
+	do {
+		direction = 0;
+		
+		// Choose direction
+		if (left != begin && right != end) {
+			// Both directions are possible
+			
+			left--;
+			right++;
+			
+			// Prioritize the values that'd result in a smaller range
+			if (minTime - left->data.time > right->data.time - maxTime) {
+				left++;
+				direction = 1;
+				current = right;
+			} else {
+				right--;
+				direction = -1;
+				current = left;
+			}
+			
+		// Not both directions are possible
+		// Go to the direction that is possible
+		} else if (left != begin) {
+			direction = -1;
+			current = left;
+		} else if (right != end) {
+			direction = 1;
+			current = right;
+		}
+		
+		// Check if the interval size would still be below maxGroupInterval if the next element would be added
+		// Abort if the interval size cannot be enlarged
+		if (direction == 1 && current->data.time - minTime > maxGroupInterval
+			|| direction == -1 && maxTime - current->data.time > maxGroupInterval)
+		{
+			direction = 0;
+		}
+		
+		if (direction != 0 && usedCamNos.count(current->data.camNo) == 0) {
+			// Check if we found an element that has completed the arrivalDelay
+			if (currentTime - current->arrivalTime > arrivalDelay) {
+				hasWaiting = true;
+			}
+			
+			if (direction == 1) {
+				// Going right, increase right interval border
+				maxTime = current->data.time;
+			} else {
+				// Going left, decrease left interval border
+				minTime = current->data.time;
+			}
+			
+			// Add to results, mark camNo as used and put CameraData into result vector
+			usedCamNos.insert(current->data.camNo);
+			result.data.push_back(current->data);
+			
+			// Result has now >= 2 elements, so it is valid
+			result.valid = true;
+			
+			// If no more other cameras available, abort
+			if (result.data.size() == camNos.size()) {
+				direction = 0;
+			}
+		}
+	} while (direction != 0);
+	
+	calculateValue(&result, minTime, maxTime, currentTime, hasWaiting);
+	
+	return result;
+}
+
+void SynchronousCameraQueue::calculateValue(Group *group, long int minTime, long int maxTime, long int currentTime, bool hasWaiting)
+{
+	long int value = 2 * maxGroupInterval - (maxTime - minTime);
+	
+	if (hasWaiting || group->data.size() == camNos.size()) {
+		value *= group->data.size();
+	}
+	
+	double timeValue = minTime - (currentTime - arrivalDelay);
+	timeValue /= maxDelay - arrivalDelay;
+	timeValue *= maxGroupInterval;
+	
+	value += (long int) timeValue;
+	
+	group->value = value;
 }
--- src/position/tracking/SynchronousCameraQueue.hpp
+++ src/position/tracking/SynchronousCameraQueue.hpp
@@ -3,10 +3,11 @@
 #include "AbstractCameraQueue.hpp"
 
 #include <list>
+#include <set>
 
 class SynchronousCameraQueue : public AbstractCameraQueue {
 public:
-	SynchronousCameraQueue(long int arrivalDelay, long int maxDelay);
+	SynchronousCameraQueue(long int arrivalDelay, long int maxDelay, long int maxGroupInterval);
 	
 	// Implementations of AbstractCameraQueue
 	size_t getSize();
@@ -23,14 +24,33 @@ private:
 		CameraData data;
 		long int arrivalTime;
 		
-		bool operator < (Bucket b) {
+		bool operator < (Bucket b)
+		{
 			return data.time < b.data.time;
 		}
 	};
 	
-	int maxDelay;
-	int arrivalDelay;
+	class Group {
+	public:
+		Group()
+		{
+			valid = false;
+			value = 0;
+		}
+		
+		bool valid;
+		long int value;
+		std::vector<CameraData> data;
+	};
+	
+	Group searchGroup(std::list<Bucket>::iterator it, long int currentTime, std::list<Bucket>::iterator begin, std::list<Bucket>::iterator end);
+	void calculateValue(Group *group, long int minTime, long int maxTime, long int currentTime, bool hasWaiting);
+	
+	long int maxDelay;
+	long int arrivalDelay;
+	long int maxGroupInterval;
 	
 	size_t size;
 	std::list<Bucket> queue;
+	std::set<int> camNos;
 };
