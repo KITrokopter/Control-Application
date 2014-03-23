926904cb4eef3c3830820ce7d88fb3bc650cb81c
Sebastian Schmidt <isibboi@gmail.com>
2014-03-23 21:02:40 +0100

    Removed autoremoval from camera queue
    It leads to errors when using a queue that under some circumstances can not remove entries

--- src/position/tracking/AbstractCameraQueue.cpp
+++ src/position/tracking/AbstractCameraQueue.cpp
@@ -14,11 +14,7 @@ void AbstractCameraQueue::enqueue(CameraData data)
 	enqueueInternal(data);
 	
 	if (getSize() > 15) {
-		ROS_WARN("Camera queue running full. Removing 10 entries");
-		
-		while (getSize() > 5) {
-			dequeue();
-		}
+		ROS_WARN("Camera queue running full. Quadcopter id: %d", data.quadcopterId);
 	}
 }
 
