53958b48f3b1e1e1a4c1fd03096fc97dbf663086
Sebastian Schmidt <isibboi@gmail.com>
2014-04-03 16:25:24 +0200

    Changed picture sending activation method.
    Instead of setting ID to 0, which can collide if a camera has ID 0, we now set the dedicated flag global to true.

--- src/position/PositionModule.cpp
+++ src/position/PositionModule.cpp
@@ -420,6 +420,7 @@ void PositionModule::setPictureSendingActivated(bool activated)
 	camera_application::PictureSendingActivation msg;
 	msg.ID = 0;
 	msg.active = activated;
+	msg.global = true;
 	
 	pictureSendingActivationPublisher.publish(msg);
 }
