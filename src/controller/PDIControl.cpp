f136541fab8ba8a368c552768fb36f9d2286cd1d
linoba <dominik.kiefer@t-online.de>
2014-04-04 10:55:08 +0200

    PDIController files added

---
+++ src/controller/PDIControl.cpp
@@ -0,0 +1,66 @@
+#include "PDIControl.hpp"
+
+PDIControl::PDIControl( double pFactor, double dFactor, double offset )
+{
+	this->pAmplification = pFactor;
+	this->pAmplificationPos = pFactor;
+	this->pAmplificationNeg = pFactor;
+	this->dAmplification = dFactor;
+	this->iAmplification = 0.0;
+	this->offset = offset;
+	this->distanceOld = 0.0;
+	this->integrator = 0.0;
+}
+
+PDIControl::PDIControl( double pFactorPos, double pFactorNeg, double dFactor, double offset )
+{
+	this->pAmplification = pFactorPos;
+	this->pAmplificationPos = pFactorPos;
+	this->pAmplificationNeg = pFactorNeg;
+	this->dAmplification = dFactor;
+	this->iAmplification = 0.0;
+	this->offset = offset;
+	this->distanceOld = 0.0;
+	this->integrator = 0.0;
+}
+
+PDIControl::PDIControl( double pFactorPos, double pFactorNeg, double dFactor, double iFactor, double offset )
+{
+	this->pAmplification = pFactorPos;
+	this->pAmplificationPos = pFactorPos;
+	this->pAmplificationNeg = pFactorNeg;
+	this->dAmplification = dFactor;
+	this->iAmplification = iFactor;
+	this->offset = offset;
+	this->distanceOld = 0.0;
+	this->integrator = 0.0;
+}
+
+double PDIControl::getManipulatedVariable(double errorSignal)
+{
+	setPAmplification( errorSignal );
+	double distanceDiff = errorSignal - distanceOld;
+	double y = this->pAmplification * (errorSignal + (this->dAmplification * distanceDiff));	
+	this->integrator += errorSignal;
+	y += this->integrator * this->iAmplification;
+	y += this->offset;	
+	this->distanceOld = errorSignal;	
+	return y;
+}
+
+double PDIControl::getAmplification()
+{
+	return this->pAmplification;
+}
+
+void PDIControl::setPAmplification( double errorSignal )
+{
+	if( errorSignal >= 0.0 )
+	{
+		this->pAmplification = this->pAmplificationPos;
+	}
+	else
+	{
+		this->pAmplification = this->pAmplificationNeg;
+	}
+}
